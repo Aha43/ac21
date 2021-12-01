3178dc85c3b60a4f51f1c1a45649264231142178
Arne Halvorsen <arne.halvorsen@gmail.com>
2021-12-01 16:05:44 +0100

    wip: added input file for first challenge and first push

---
+++ 21/1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var hc = new HttpClient();
+var r = await hc.GetAsync("https://adventofcode.com/2021/day/1/input")
+    .ConfigureAwait(continueOnCapturedContext: false);
+if (r.IsSuccessStatusCode)
+{
+    var input = r.Content.ReadAsStringAsync().ConfigureAwait(continueOnCapturedContext: false);
+}
+else
+{
+    Console.WriteLine("Input read failed");
+    Console.WriteLine(r.StatusCode);
+}
