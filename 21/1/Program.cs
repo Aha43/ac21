b8e2c3be22fca35fc30f95c556a5c99cc63ebcc9
Arne Halvorsen <arne.halvorsen@gmail.com>
2021-12-01 19:00:14 +0100

    feat: Both challenges done

--- 21/1/Program.cs
+++ 21/1/Program.cs
@@ -1,5 +1,5 @@
 ﻿var hc = new HttpClient();
-var inputUri = (args.Length == 0 || args[0] != "e") ? "https://raw.githubusercontent.com/Aha43/ac21/main/21/1/Input.txt" : "https://raw.githubusercontent.com/Aha43/ac21/main/21/1/example.txt";
+var inputUri = (args[0] == "i") ? "https://raw.githubusercontent.com/Aha43/ac21/main/21/1/Input.txt" : "https://raw.githubusercontent.com/Aha43/ac21/main/21/1/example.txt";
 var r = await hc.GetAsync(inputUri)
     .ConfigureAwait(continueOnCapturedContext: false);
 if (r.IsSuccessStatusCode)
@@ -9,6 +9,18 @@ if (r.IsSuccessStatusCode)
     var c = 0;
     if (n > 1)
     {
+        if (args[1] == "2")
+        {
+            input = input.Select((e, i) => 
+            { 
+                var sum = 0;
+                var m = i + 3;
+                m = (m < n) ? m : n;
+                for (int j = i; j < m; j++) sum += input[j];
+                return sum; 
+            }).ToArray();
+        }
+        
         for (var i = 1; i < n; i++)
         {
             if (input[i] > input[i-1]) c++;
