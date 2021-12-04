092df3165a742bf4b682d6ef75fe59ceeef687ea
Arne Halvorsen <arne.halvorsen@gmail.com>
2021-12-04 10:30:51 +0100

    wip: Part two done for example but got bug

--- 21/3/Program.cs
+++ 21/3/Program.cs
@@ -16,7 +16,7 @@ var numbers = input.Select(e =>
         retVal[i] = e[i] == '0' ? 0 : 1; 
     }
     return retVal;
-});
+}).ToArray();
 
 var sums = new int[size];
 foreach (var e in numbers)
@@ -43,8 +43,42 @@ for (var i = 0; i < size; i++)
     }
 }
 
-
 var gammaRate = Convert.ToInt32(string.Join("", gammaRateBin), 2);
 var epsilonRate = Convert.ToInt32(string.Join("", epsilonRateBin), 2);
 
-Console.WriteLine(gammaRate * epsilonRate);
+Console.WriteLine("Part one: " + (gammaRate * epsilonRate));
+
+var gammaIteration = numbers.Where(e => e[0] == gammaRateBin[0]).ToArray();
+var gi = 1;
+while (gammaIteration.Count() > 1 && gi < size)
+{
+    var ones = sums[gi];
+    var zeros = n - ones;
+    var criteria = (ones >= zeros) ? 1 : 0;
+    //gammaIteration = gammaIteration.Where(e => e[gi] == gammaRateBin[gi]).Select(e => e).ToArray();
+    gammaIteration = gammaIteration.Where(e => e[gi] == criteria).Select(e => e).ToArray();
+    gi++;
+}
+
+var epsilonIteration = numbers.Where(e => e[0] == epsilonRateBin[0]).ToArray();
+var ei = 1;
+while (epsilonIteration.Count() > 1 && ei < size)
+{
+    var ones = sums[ei];
+    var zeros = n - ones;
+    var criteria = (zeros <= ones) ? 0 : 1;
+    //epsilonIteration = epsilonIteration.Where(e => e[ei] == epsilonRateBin[ei]).ToArray();
+    epsilonIteration = epsilonIteration.Where(e => e[ei] == criteria).ToArray();
+    ei++;
+}
+
+var oxRatingBin = gammaIteration.FirstOrDefault();
+var co2RatingBin = epsilonIteration.FirstOrDefault();
+
+if (oxRatingBin != null && co2RatingBin != null)
+{
+    var oxRating = Convert.ToInt32(string.Join("", oxRatingBin), 2);
+    var co2Rating = Convert.ToInt32(string.Join("", co2RatingBin), 2);
+
+    Console.WriteLine($"Part Two: {oxRating * co2Rating}");
+}
